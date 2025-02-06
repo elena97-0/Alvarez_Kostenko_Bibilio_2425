a8e97932977f28005eebc4f154f06473642ffc38
elena97-0 <lenadefacto@hotmail.com>
2025-02-06 11:15:27 +0100

    creación_clase_biblioteca

---
+++ Biblioteca/Biblioteca/src/Biblioteca.java
@@ -0,0 +1,39 @@
+public class Biblioteca {
+    private int numLibros;
+    private int numUsuarios;
+    private Usuario [] arrayUsuarios;
+    private Libro[] arrayLibros;
+    /*constructor */
+    public Biblioteca(int numLibros, int numUsuarios, Usuario[] arrayUsuarios, Libro[] arrayLibros) {
+        this.numLibros = numLibros;
+        this.numUsuarios = numUsuarios;
+        this.arrayUsuarios = arrayUsuarios;
+        this.arrayLibros = arrayLibros;
+    }
+    public int getNumLibros() {
+        return numLibros;
+    }
+    public void setNumLibros(int numLibros) {
+        this.numLibros = numLibros;
+    }
+    public int getNumUsuarios() {
+        return numUsuarios;
+    }
+    public void setNumUsuarios(int numUsuarios) {
+        this.numUsuarios = numUsuarios;
+    }
+    public Usuario[] getArrayUsuarios() {
+        return arrayUsuarios;
+    }
+    public void setArrayUsuarios(Usuario[] arrayUsuarios) {
+        this.arrayUsuarios = arrayUsuarios;
+    }
+    public Libro[] getArrayLibros() {
+        return arrayLibros;
+    }
+    public void setArrayLibros(Libro[] arrayLibros) {
+        this.arrayLibros = arrayLibros;
+    }
+
+    
+}
