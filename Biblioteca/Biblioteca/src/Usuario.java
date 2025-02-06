f9567f7d241b8867aad0c733be1517d60a405dd0
Dani55Alv <alvarezmdanielel62@gmai.com>
2025-02-06 10:05:45 +0100

    Primer_Commit

---
+++ Biblioteca/Biblioteca/src/Usuario.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Es la clase donde se almacena las caracteristicas del usuario.
+ * 
+ * @author Daniel Alvarez Morales
+ * @version 1.0
+ * @since 2025
+ * 
+ */
+public class Usuario {
+ 
+    private String nombreUsuario;
+    private String contrasenia;
+
+}
