c887e65e48a1d92d511726cbd658f39f95057c08
elena97-0 <lenadefacto@hotmail.com>
2025-02-14 21:31:57 +0100

    comentarios

--- Biblioteca/Biblioteca/src/Usuario.java
+++ Biblioteca/Biblioteca/src/Usuario.java
@@ -5,6 +5,11 @@
  * @version 1.0
  * @since 2025
  * 
+ * @author Elena Kostenko
+ * @version 2.0
+ * @since 2025
+ * 
+ * 
  */
 public class Usuario {
 
@@ -18,22 +23,48 @@ public class Usuario {
         this.dni = dni;
     }
 
+    
+    /** Es un método que devuelve el nombre de usuario
+     * @return String Nombre de usuario
+     */
     public String getNombreUsuario() {
         return nombreUsuario;
     }
 
+    
+    /**
+     * Es un método que devuelve la contraseña de usuario
+     * 
+     * @return String de contraseña
+     */
     public String getContrasenia() {
         return contrasenia;
     }
 
+    
+    /**
+     * Es un método que devuelve el DNI de usuario
+     * 
+     * @return String de DNI de usuario
+     */
     public String getDni() {
         return dni;
     }
 
+    
+    /**
+     * Es una función que pasa por parámetro el nombre de usuario 
+     * 
+     * @param nombreUsuario nombre
+     */
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
 
+    
+    /** Es una función que pasa por parámetro la contraseña
+     * @param contrasenia
+     */
     public void setContrasenia(String contrasenia) {
         this.contrasenia = contrasenia;
     }
