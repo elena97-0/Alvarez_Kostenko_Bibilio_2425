c8947eb8b9b77e4e8d28a82ef95c33107ca9ec8d
elena97-0 <lenadefacto@hotmail.com>
2025-02-14 11:44:42 +0100

    gestion_usuario_elena

--- Biblioteca/Biblioteca/src/Biblioteca.java
+++ Biblioteca/Biblioteca/src/Biblioteca.java
@@ -1,43 +1,139 @@
 public class Biblioteca {
     private int numLibros;
     private int numUsuarios;
-    private Usuario [] arrayUsuarios;
+    private Usuario[] arrayUsuarios;
     private Libro[] arrayLibros;
-    /*constructor */
-    public Biblioteca(int numLibros, int numUsuarios, Usuario[] arrayUsuarios, Libro[] arrayLibros) {
+    private Libro[] arraylibrosPrestados;
+    private int nLibrosPrestados;
+
+    /* constructor */
+    public Biblioteca(int numLibros, int numUsuarios, Usuario[] arrayUsuarios, Libro[] arrayLibros,
+            int nLibrosPrestados, Libro[] arraylibrosPrestados) {
         this.numLibros = numLibros;
         this.numUsuarios = numUsuarios;
         this.arrayUsuarios = arrayUsuarios;
         this.arrayLibros = arrayLibros;
+        this.arraylibrosPrestados = arraylibrosPrestados;
+        this.nLibrosPrestados = nLibrosPrestados;
     }
+
     public int getNumLibros() {
         return numLibros;
     }
+
     public void setNumLibros(int numLibros) {
         this.numLibros = numLibros;
     }
+
     public int getNumUsuarios() {
         return numUsuarios;
     }
+
     public void setNumUsuarios(int numUsuarios) {
         this.numUsuarios = numUsuarios;
     }
+
     public Usuario[] getArrayUsuarios() {
         return arrayUsuarios;
     }
+
     public void setArrayUsuarios(Usuario[] arrayUsuarios) {
         this.arrayUsuarios = arrayUsuarios;
     }
-    public Libro[] getArrayLibros() { 
+
+    public Libro[] getArrayLibros() {
         return arrayLibros;
     }
+
     public void setArrayLibros(Libro[] arrayLibros) {
         this.arrayLibros = arrayLibros;
     }
 
-    //cambio
-    //gfrhggjhjh
-    //hffhhfue
-    //fhje
-    //ffhhgj
+    private boolean checkeadorArraysUsuarios(String nombreUsuario) {
+        boolean noExiste = true;
+        for (int i = 0; i < this.arrayUsuarios.length && noExiste; i++) {
+            if (arrayUsuarios[i].getNombreUsuario().equals(nombreUsuario)) {
+                noExiste = false;
+
+            }
+        }
+
+        return noExiste;
+    }
+
+    private boolean checkeadorArraysLibrosPrestados(int isbn) {
+        boolean noExiste = true;
+        for (int i = 0; i < this.arraylibrosPrestados.length && noExiste; i++) {
+            if (arraylibrosPrestados[i].getIsbn() == (isbn)) {
+                noExiste = false;
+
+            }
+        }
+
+        return noExiste;
+
+    }
+
+    private boolean checkeadorArraysLibros(int isbn) {
+        boolean noExiste = true;
+        for (int i = 0; i < this.arrayLibros.length && noExiste; i++) {
+            if (arrayLibros[i] != null && arrayLibros[i].getIsbn() == (isbn)) {
+                noExiste = false;
+
+            }
+        }
+
+        return noExiste;
+
+    }
+
+
+    public void AgregarLibroNuevo(Usuario usuario) {
+
+        if (numUsuarios < this.arrayUsuarios.length) {
+            this.arrayUsuarios[numUsuarios] = usuario;
+            numUsuarios++;
+        } else {
+            System.out.println("No caben mas usuarios (Está lleno la biblioteca de usuarios)");
+        }
+
+    }
+
+    public void registrarUsuario(String nombreUsuario, String dni, String contrasenia) {
+        Usuario usuario = new Usuario(null, null, null);
+        usuario.setNombreUsuario(nombreUsuario);
+        usuario.setContrasenia(contrasenia);
+        usuario.setDni(dni);
+        if (numUsuarios < this.arrayUsuarios.length) {
+            this.arrayUsuarios[numUsuarios] = usuario;
+            numUsuarios++;
+        } else {
+            System.out.println("No caben mas usuarios (Está lleno la biblioteca)");
+        }
+    }
+    
+    public void mostrarInfUsuarioRegistrado() {
+        boolean noExiste = true;
+        for (int i = 0; i < getNumUsuarios(); i++) {
+            arrayUsuarios[i].getDni();
+            noExiste = false;
+            System.out.println("El usuario con nombre " + arrayUsuarios[i].getNombreUsuario() + " y el dni "
+                    + arrayUsuarios[i].getDni() + " tiene la contraseña " + arrayUsuarios[i].getContrasenia());
+
+        }
+        if (noExiste) {
+            System.out.println("No existen usuarios para mostrar");
+        }
+    }
+        
+    public void agregarUsuario(Usuario usuario) {
+
+        if (numUsuarios < this.arrayUsuarios.length) {
+            this.arrayUsuarios[numUsuarios] = usuario;
+            numUsuarios++;
+        } else {
+            System.out.println("No caben mas usuarios (Está lleno la biblioteca)");
+        }
+
+    }
 }
--- Biblioteca/Biblioteca/src/Usuario.java
+++ Biblioteca/Biblioteca/src/Usuario.java
@@ -1,5 +1,3 @@
-
-
 /**
  * Es la clase donde se almacena las caracteristicas del usuario.
  * 
@@ -9,8 +7,39 @@
  * 
  */
 public class Usuario {
- 
+
     private String nombreUsuario;
     private String contrasenia;
+    private String dni;
+
+    public Usuario(String nombreUsuario, String contrasenia, String dni) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+        this.dni = dni;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 
 }
